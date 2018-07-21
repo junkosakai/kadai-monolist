107577931bb4c7cc891a06f7a8432e28647ddf8c
junko.sakai <myjn28@gmail.com>
2018-07-21 08:17:25 +0000

    have

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,3 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
 use \App\Item;
 
   class ItemsController extends Controller
@@ -27,22 +33,32 @@ use \App\Item;
                 $items[] = $item;
             }
         }
+        
+        
+        
+        
 
         return view('items.create', [
             'keyword' => $keyword,
             'items' => $items,
         ]);
+        
     }
     
     public function show($id)
     {
       $item = Item::find($id);
       $want_users = $item->want_users;
+      $have_users = $item->have_users;
 
       return view('items.show', [
           'item' => $item,
           'want_users' => $want_users,
+          'have_users' => $have_users,
       ]);
       
-    }  
+    }    
+      
   }
+  
+  
--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -8,16 +8,20 @@
                             <img src="{{ $item->image_url }}" alt="" class="">
                         </div>
                         <div class="panel-body">
-                            @if ($item->id)
-                                <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
-                            @else
-                                <p class="item-title">{{ $item->name }}</p>
-                            @endif
+                                    @if ($item->id)
+                                        <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                                       @else
+                                        <p class="item-title">{{ $item->name }}</p>
+                                    @endif
                             <div class="buttons text-center">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
                                 @endif
-                            </div>
+                                @if (Auth::check())
+                                    @include('items.have_button', ['item' => $item])
+                                @endif
+
+                            </div> 
                         </div>
                     </div>
                 </div>
